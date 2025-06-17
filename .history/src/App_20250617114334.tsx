5c9dc47c7cd791c289b9216c736b384f0d3c5cb3
Yhs-DalC <scvthego@gmail.com>
2025-06-17 15:30:40 +0900

    feat: board front

---
+++ .history/src/App_20250617114334.tsx
@@ -0,0 +1,51 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Board from './views/board'
+import SignUp from './views/auth/SignUp'
+import Index from '.'
+import MemberSignUp from './views/auth/MemberSignUp'
+import TrainerSignUp from './views/auth/TrainerSignUp'
+import TrainerCareer from './views/trainer/TrainerCareer'
+import Login from './views/auth/Login'
+
+import TrainerCouponList from './views/coupon/TrainerCouponList'
+
+import TrainerList from './views/admin/TrainerList'
+
+
+//! 프로젝트 기초 환경 설정
+// 1. 외부 라이브러리 설치(의존성 설치)
+// - axios
+// - react-router-dom
+// - zustand
+// npm i 각 라이브러리명
+
+// 2. 공통 상수 공통 함수, 공통 타입 명시
+
+// 3. root 경로의 index.tsx에서 BrowserRouter 등록
+
+// 4. 폴더 구조 생성
+/// assets, apis(ts), components(tsx), constants(ts), layouts(tsx), stores(ts), styles(ts), types(ts), views(tsx)
+
+// ts - export const tmp = '';
+// tsx - rfce(함수형 컴포넌트 생성)
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Index />} />
+        <Route path='/auth/sign-up' element={<SignUp />} />
+        <Route path='/auth/sign-up/member' element={<MemberSignUp />} />
+        <Route path='/auth/sign-up/trainer' element={<TrainerSignUp />} />
+        <Route path='/auth/login' element={<Login />} />
+        <Route path='/admin/trainers' element={<TrainerList />} />
+        <Route path='/personal-community-boards' element={<Board />} />
+        <Route path='/users/trainers/me/career' element={<TrainerCareer />} />
+        <Route path='/users/trainers/me/coupons' element={<TrainerCouponList/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
---
+++ .history/src/views/header/HeaderStyle_20250617135808.ts
@@ -0,0 +1,127 @@
+import { css } from "@emotion/react";
+
+export const headerStyle = css`
+  width: 100%;
+  height: 180px;
+  margin-bottom: 15px;
+  margin-right: 15px;
+  background-color: white;
+  border-bottom: 3px solid #437bc0;
+  box-sizing: border-box;
+`;
+
+export const headerUp = css`
+  width: 100%;
+  height: 100px;
+  margin: auto;
+
+  border-bottom: 3px solid #75a7ef;
+  box-sizing: border-box;
+`;
+
+export const headerUpSet = css`
+  width: 60%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const logoStyle = css`
+  width: 200px;
+  height: 100px;
+  background-image: url("/fitmatelogo.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+export const loginStyle = css`
+  width: 250px;
+  height: 100px;
+  font-size: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 100px;
+  font-size: 16px;
+`;
+
+export const loginProfile = css`
+  width: 60px;
+  height: 60px;
+  background-image: url("/profileImageSample.png");
+  background-size: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  overflow: hidden;
+`;
+
+export const loginStyleBtn = css`
+  width: 50px;
+  height: 50px;
+
+  text-decoration: none;
+  border-style: none;
+`;
+
+export const linkStyle = css`
+  text-decoration: none;
+  color: #3f4756;
+  margin: 0 4px;
+`;
+
+export const dividerStyle = css`
+  margin: 0 6px;
+  color: #ccc;
+`;
+
+export const headerNav = css`
+  width: 70%;
+  height: 80px;
+  margin: auto;
+  display: flex;
+  justify-content: space-around;
+  cursor: pointer;
+`;
+
+export const headerNavDivs = css`
+  font-size: 32px;
+  line-height: 80px;
+  color: #699ce4;
+`;
+
+
+export const headerStyle = css`
+    width: 100%;
+    height: 100px;
+    box-sizing: border-box;
+`
+
+export const userName = css`
+    height: 100px;
+    font-size: 16px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+export const click = css`
+    cursor: pointer;
+`
+
+
+export const logoutStyle =css`
+    width: 250px;
+    height: 100%;
+    font-size: 20px;
+    display: flex;
+    justify-content: end;
+`
+export const logoutStyleA = css`
+    margin-right: 10px;
+    text-decoration: none;
+
+
+`
