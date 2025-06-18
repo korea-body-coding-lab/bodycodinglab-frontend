e62541cd11482c8e9885b84a60f3c2d4b1792c61
mijinn0 <pmj_0818@naver.com>
2025-06-18 22:19:52 +0900

    feat: 사용자 정보 조회 및 수정 구현

---
+++ src/dtos/user/response/get-member-info.response.dto.ts
@@ -0,0 +1,10 @@
+export interface GetMemberInfoResponseDto {
+  username: string;
+  name: string;
+  birthdate: string;
+  gender: string;
+  phone: string;
+  email: string;
+  memberAddress: string;
+    // profileImageUrl: string;
+}
